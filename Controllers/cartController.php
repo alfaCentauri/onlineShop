81893014d7d08b37f24b1c773e2d038a4f159f7a
RICARDO PRESILLA <ricardopresilla@gmail.com>
2019-06-13 13:32:51 -0430

    Depurado comentario.

--- Controllers/cartController.php
+++ Controllers/cartController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2019 ricardo
+ * Copyright (C) 2019 Ingeniero en Computación: Ricardo Presilla.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
