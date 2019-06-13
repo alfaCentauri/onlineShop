ccfff3766dd4a5b2f77a9b0de362f069b56bba5a
Ricardo Presilla <ricardopresilla@gmail.com>
2019-06-13 17:15:17 -0400

    Creado el metodo de agregar.

--- Controllers/qualificationController.php
+++ Controllers/qualificationController.php
@@ -39,6 +39,10 @@ class qualificationController implements Crud
      */
     private $qualification;
     /**
+     * Contains the status of the request.
+     */
+    private $statusCode;
+    /**
      * Qualification constructor.
      */
     public function __construct()
@@ -46,6 +50,7 @@ class qualificationController implements Crud
         $this->user = new Users();
         $this->product = new Product();
         $this->qualification = new Qualificationes();
+        $this->statusCode = 400;
     }
 
     public function index()
@@ -53,10 +58,56 @@ class qualificationController implements Crud
         $data = $this->qualification->listAverage();
         return $data;
     }
-
+    /**
+     * Add a score to a product. Receive the data through a JSON and the post method.
+     *
+     */
     public function add()
     {
-        // TODO: Implement add() method.
+        $node = array();
+        if ($_POST)
+        {
+            $content=$_POST['json'];
+            $data = json_decode($content, true);
+            if(isset($data))
+            {
+                if ($data['idUser']>0 && $data['idProduct'] >0 && $data['points']>0)
+                {
+                    $this->qualification->setIdUser($data['idUser']);
+                    $this->qualification->setIdProduct($data['idProduct']);
+                    $this->qualification->setPoints($data['points']);
+                    $id=$this->qualification->add();
+                    if ($id>0)
+                    {
+                        $this->statusCode = 201;
+                        $node['id']=$id;
+                    }
+                    else
+                    {
+                        $this->statusCode = 409;
+                        $node['error']="Error saving the score.";
+                    }
+                }
+                else
+                {
+                    $this->statusCode = 406;
+                    $node['error']="Not Acceptable.";
+                }
+            }
+            else
+            {
+                $this->statusCode = 400;
+                $node['error']="Request error. :(";
+            }
+        }
+        else
+        {
+            $this->statusCode = 400;
+            $node['error']="Request error. :(";
+        }
+        // Response
+        header('Content-Type', 'application/json', $this->statusCode);
+        echo json_encode($node);
     }
 
     public function view(int $id = 0)
